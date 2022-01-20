58f45ecd4cf134c2035eae01511918fabb3d7c2c
Eugene Blokhin <eugene.blokhin@gmail.com>
2022-01-20 15:49:20 +0100

    Fixed script for building in Docker

--- build-in-docker.ps1
+++ build-in-docker.ps1
@@ -1,6 +1,6 @@
 $docsroot = Resolve-Path $PSScriptRoot
 Write-Information -Message "Building documentation in folder ${docsroot}" -InformationAction Continue
-docker run --rm -v ${docsroot}:/docs firely/docs-sphinx
+docker run --rm -v ${docsroot}:/docs firely.azurecr.io/firely/docs-sphinx:latest
 $index = "${docsroot}\_build\html\index.html"
 Write-Information -Message "Build ready, showing output from ${index}" -InformationAction Continue
 explorer.exe ${index}

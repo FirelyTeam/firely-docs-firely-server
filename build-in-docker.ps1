a178fffbff7e441cd193098df12e3a737ed1c81f
richfirely <rich@fire.ly>
2023-01-12 08:05:45 -0500

    pr fixes

--- build-in-docker.ps1
+++ build-in-docker.ps1
@@ -1,11 +1,7 @@
 $docsroot = Resolve-Path $PSScriptRoot
 Write-Information -Message "Building documentation in folder ${docsroot}" -InformationAction Continue
 docker run --rm -v ${docsroot}:/docs firely.azurecr.io/firely/docs-sphinx:latest
-if ($LASTEXITCODE -ne 0) {
-    Write-Error -Message "Docker run failed. Is Docker active?" -RecommendedAction "Start Docker Desktop" -InformationAction Stop
-    Exit
-}
 $index = "${docsroot}\_build\html\index.html"
 Write-Information -Message "Build ready, showing output from ${index}" -InformationAction Continue
-explorer.exe ${index}
+#explorer.exe ${index}
 Write-Information -Message "Done" -InformationAction Continue
